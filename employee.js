c85f5d8033afbb75113fd87a25af333c41a7e988
Leeroy Phili <leeroyphili@gmail.com>
2020-04-23 14:20:21 -0400

    add sql schema and created a connection the database

---
+++ employee.js
@@ -0,0 +1,46 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Stembiso369",
+  database: "employee_trackerDB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+
+function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "what would you like to do ?",
+        choices: ["POST", "BID", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === "POST") {
+          postAuction();
+        }
+        else if(answer.postOrBid === "BID") {
+          bidAuction();
+        } else{
+          connection.end();
+        }
+      });
+  }
